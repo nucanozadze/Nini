0553bca6515369a8025043e74eb516e1419f1a6a
Nini <138128778+nucanozadze@users.noreply.github.com>
2023-07-14 22:43:36 +0400

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,50 @@
+name = "gio"
+surname = "nozadze"
+age =13
+country = "georgia"
+city = "surami"
+school = "GOA"
+playing_games =False
+playing_in_FC_iveria = True
+shirt_number = 11
+height=165
+print(name + surname)
+print(name + school)
+print(name + city)
+print(name + country)
+print(name + str(height) )
+print(name + str(age))
+print(name +name)
+print(name + city + country)
+print(name + school + city)
+print(name + surname + school)
+ggdygy gyygsgyfugey gg usgusg 
+egysg yusguygesygey 
+ esgtsi  du dutf 
+ ft uu fufu
+uf fuu uu f UnboundLocalErrordutuf tfu
+u f ufuf  ufu tupleu filterutfhui
+heiuueg
+euguigw\hweuuigw   
+ uhugrudihuh udirh 
+ uu rueryie 
+e ur grg eyg yield ihiuieiu
+e hegyugeyur
+e tgeiugtiueg
+e ggeyyrggesgsi if u\w gygygw ty
+ euueiug  uuwi 
+ weuguiwe   u gwue
+ egweygygt
+ uiweuiuieg bjkcbxk  
+ uiggiug  i   udhgius
+ gyg yusg fbv\ uidi bv
+ uhuihsuhiu   
+gs hguidh uhiu \  gui
+ ehuighuigh
+ uhhuihie
+ huie hiuhiu
+ eugyigi
+ eui rhuieuhie 
+ hieu UnicodeError hieriuh ImportError
+ ehiuiehigue global ehyeigi\
+ e gigigiyre 
